aa9b7fe0b7f93c510731c731abeec848726a77b2
Jacek <jtarzanowski@gmail.com>
2023-04-04 18:39:09 +0200

    need to comment

--- backend/imagehub/models.py
+++ backend/imagehub/models.py
@@ -29,7 +29,7 @@ class Post_like(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
-class Comment_like(models.Model): # Klasy nazywamy dużymi literami np. CommnetLike
+class Comment_like(models.Model):
     like_id = models.IntegerField(primary_key=True)
     like_type = models.BooleanField()
     user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
