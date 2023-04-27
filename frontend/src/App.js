e594387a2f2ed6e283cb2d556e22527665ec01ad
Stanisław Olszak <solszakdev@gmail.com>
2023-04-27 22:00:28 +0200

    adding set Token service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,43 @@
-import Login from "./components/Login";
 import Register from "./components/Register";
-import RecoverPassword from "./components/RecoverPassword";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// import RecoverPassword from "./components/RecoverPassword";
+// import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import useToken from "./components/useToken";
+import Dashboard from "./components/Dashboard/Dashboard"
+
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
+
+// function getToken() {
+//   const tokenSring = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenSring);
+//   return userToken?.token;
+// }
 
 const App = () => {
+  // const token = getToken();
+  const {token, setToken} = useToken();
+
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
+
+
+
   return (
-    <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/password" element={<RecoverPassword />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-  </Router>
-  </div>
+      <div className="App">
+        <h1>Main page</h1>
+        <p>{()=>{JSON.stringify(token)}}</p>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login setToken={setToken} />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </div>
   );
 };
 
