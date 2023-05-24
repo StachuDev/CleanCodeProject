e61d4193832140df25da5d96249f158c26b41265
Stanisław Olszak <solszakdev@gmail.com>
2023-05-24 16:43:27 +0200

    adding route to adding post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import Register from "./components/Register";
 import RecoverPassword from "./components/RecoverPassword";
 import Homepage from "./components/Homepage";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UploadPost from "./components/UploadPost";
+import Login from "./components/Login";
+
 
 const App = () => {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        {/* <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/password" element={<RecoverPassword />} />
-        <Route path="/login" element={<Login />} /> */}
-        <Route path="/homepage" element={<Homepage/>}/>
-      </Routes>
-  </Router>
-  </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/password" element={<RecoverPassword />} />
+          <Route path="/login" element={<Login />} /> */}
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/create" element={<UploadPost />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
